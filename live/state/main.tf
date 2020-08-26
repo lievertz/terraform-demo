7890e40dd6724280943605776553d933ea354108
Matt Lievertz <lievertz@users.noreply.github.com>
2020-08-26 11:47:34 -0400

    state - added finalized state state

--- live/state/main.tf
+++ live/state/main.tf
@@ -11,7 +11,7 @@ provider "aws" {
 }
 
 module "s3_backend" {
-  source                         = "git@github.com:lievertz/terraform-demo.git/modules/backend?ref=v0.0.1"
+  source                         = "git@github.com:lievertz/terraform-demo.git//modules/backend?ref=v0.0.1"
   tf_state_aws_s3_bucket_name    = var.tf_state_aws_s3_bucket_name
   tf_state_aws_dynamo_table_name = var.tf_state_aws_dynamo_table_name
 }
